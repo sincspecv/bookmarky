5d2b6a4b751ffa0d50283403210d3173e5be3b0c
Matthew Schroeter <schroeter.m83@gmail.com>
2023-07-20 15:17:41 -0500

    fixed spacing of elements in link component so everything lines up equally on each link

--- src/newtab/components/WorkspaceColumnLink.vue
+++ src/newtab/components/WorkspaceColumnLink.vue
@@ -76,15 +76,15 @@ onMounted(() => {
     >
         <div class="flex justify-start flex-nowrap gap-5 w-full max-w-full" @click.prevent.self="openLink">
             <div class="avatar" @click.prevent="openLink">
-                <figure class="w-20 rounded" v-if="props.link.favIconUrl">
-                    <img class="!object-contain" :src="props.link.favIconUrl" />
+                <figure class="w-20 rounded">
+                    <img class="!object-contain" :src="props.link.favIconUrl" v-if="props.link.favIconUrl" />
                 </figure>
             </div>
             <div class="flex flex-col text-left overflow-hidden"  @click.prevent="openLink">
                 <h3 class="normal-case text-bold text-sm truncate text-ellipses">{{props.link.title}}</h3>
                 <span class="normal-case text-xs overflow-hidden truncate text-ellipses">{{props.link.url}}</span>
             </div>
-            <div class="dropdown dropdown-bottom dropdown-end h-auto self-center">
+            <div class="dropdown dropdown-bottom dropdown-end h-auto ml-auto mr-0 self-center">
                 <label
                     tabindex="0"
                     class="btn btn-ghost px-3 hover:btn-neutral btn-sm opacity-25 hover:opacity-100"
